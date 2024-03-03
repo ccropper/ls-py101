b7456b78fa488704232ce75bea7a8d755c9aa9f1
caiqian.cropper <caiqian.cropper@zapier.com>
2024-03-03 11:52:11 -0600

    lesson 3 exercises

---
+++ lesson_3/ascii.py
@@ -0,0 +1,8 @@
+# For this practice problem, write a program that outputs The 
+# Flintstones Rock! 10 times, with each line indented 1 space to the 
+# right of the line above it. 
+
+phrase = "The Flintstones Rock!"
+
+for space in range(0,10):
+    print((' ' * space) + phrase)
